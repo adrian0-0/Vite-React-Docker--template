33272c7aa202a614c18f01dcd391ac7603102df6
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2023-03-05 15:46:13 -0300

    refactor: remove server from viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 8080,
-  },
+  // server: {
+  //   host: true,
+  //   port: 8080,
+  // },
   base: '/Vite-React-Docker--template/',
   plugins: [react()],
 });
